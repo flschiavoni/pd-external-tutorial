e02a4ce65423c7ce3131ed79b20bf80d056fc3e1
Flavio Schiavoni <fls@rendera.com.br>
2011-11-15 14:31:58 -0200

    New examples

--- examples/src/example14.c
+++ examples/src/example14.c
@@ -29,6 +29,9 @@ void example14_destroy(t_example14 *x) {
    -------------------------------------------------------------------------------------------------------------------------------------------*/
 // THE BOUNDING RECTANGLE
 static void my_getrect(t_gobj *z, t_glist *glist, int *xp1, int *yp1, int *xp2, int *yp2){
+	// This function is always called. 
+	// Better do not put a post here...
+	// post("GETRECT");
 	t_example14 *x = (t_example14 *)z;
  	*xp1 = x->x_obj.te_xpix;
  	*yp1 = x->x_obj.te_ypix;
@@ -44,6 +47,8 @@ static void my_vis(t_gobj *z, t_glist *glist, int vis){
 	t_canvas * canvas = glist_getcanvas(glist);
 
 	if(vis){ // VISIBLE
+	post("VISIBLE");
+
         sys_vgui(".x%lx.c create rectangle %d %d %d %d -tags %xrr -fill #FF0000\n",
 		glist_getcanvas(glist),
 		x->x_obj.te_xpix,
@@ -52,15 +57,31 @@ static void my_vis(t_gobj *z, t_glist *glist, int vis){
 		x->x_obj.te_ypix + 50,
 		x
 		);
-
+       sys_vgui(".x%x.c create text %d %d -text {example14} -anchor w  -tags %xlb\n",
+                canvas,
+		x->x_obj.te_xpix + 2,
+		x->x_obj.te_ypix + 12,
+		x);
+	sys_vgui("checkbutton .x%x.c.s%xcb -foreground blue -background yellow -command do_something\n", canvas,x);
+	sys_vgui(".x%x.c create window %d %d -anchor nw -tags %xbb\n",
+		canvas,
+		x->x_obj.te_xpix + 70,
+		x->x_obj.te_ypix + 120,
+		canvas,
+		x,
+		x);
+	sys_vgui("%xbb add .x%x.c.s%xcb",x,canvas,x);
 	}else{ // INVISIBLE
+		post("INVISIBLE");
 		sys_vgui(".x%x.c delete %xrr\n",canvas, x);
+		sys_vgui(".x%x.c delete %xlb\n",canvas, x);
 	}
 //	canvas_fixlinesfor(glist, (t_text *)x);
 }
 
 // DISPLACE IT
 void my_displace(t_gobj *z, t_glist *glist,int dx, int dy){
+	post("MOVED");
 	t_canvas * canvas = glist_getcanvas(glist);
 	t_example14 *x = (t_example14 *)z;
 	x->x_obj.te_xpix += dx; // x movement
@@ -75,6 +96,7 @@ void my_displace(t_gobj *z, t_glist *glist,int dx, int dy){
 		x->x_obj.te_ypix + 50
 		);
         sys_vgui(".x%x.c coords %xrr %d %d %d %d\n",canvas,x,x->x_obj.te_xpix,x->x_obj.te_ypix,x->x_obj.te_xpix + 70,x->x_obj.te_ypix + 50);
+        sys_vgui(".x%x.c coords %xlb %d %d \n",canvas,x,x->x_obj.te_xpix + 2, x->x_obj.te_ypix + 12);
 
 	canvas_fixlinesfor(glist, (t_text *)x);
 }
@@ -88,6 +110,7 @@ static void my_activate(t_gobj *x, struct _glist *glist, int state){
 static void my_select(t_gobj *z, t_glist *glist, int state){
  	t_example14 *x = (t_example14 *)z;
  	if (state) {
+	post("SELECTED");
         sys_vgui(".x%x.c create rectangle %d %d %d %d -tags %xSEL -outline blue\n",
 		glist_getcanvas(glist),
 		x->x_obj.te_xpix,
@@ -97,13 +120,14 @@ static void my_select(t_gobj *z, t_glist *glist, int state){
 		x
 		);
 	}else {
+	post("DESELECTED");
  	sys_vgui(".x%x.c delete %xSEL\n",glist_getcanvas(glist), x);
 	}
 }
 
 static int my_click(t_gobj *z, struct _glist *glist, int xpix, int ypix, int shift, int alt, int dbl, int doit){
 	post("Clicked xpix:%d ypix:%d shift:%d alt:%d dbl:%d doit:%d", xpix,ypix, shift, alt, dbl, doit);
-	return (1);
+	return 0;
 }
 
 static void my_delete(t_gobj *z, t_glist *glist){
@@ -111,6 +135,14 @@ static void my_delete(t_gobj *z, t_glist *glist){
 	canvas_deletelinesfor(glist_getcanvas(glist), x);
 	post("Object deleted!");
 } 
+
+void my_save(t_gobj *c, t_binbuf *b){
+	post("SAVE");
+}
+
+void my_properties(t_gobj *c, t_glist *list){
+	post("PROPERTIES");
+}
 
 void example14_setup(void) {
     example14_class = class_new(gensym("example14"),
@@ -124,10 +156,14 @@ void example14_setup(void) {
     widgetbehavior.w_getrectfn = my_getrect; // The external GUI rectangle definition
     widgetbehavior.w_visfn= my_vis; //How to make ir visible / invisible
     widgetbehavior.w_displacefn= my_displace; //what to do whe moved
-    widgetbehavior.w_selectfn= my_select, // What to do when selected
+    widgetbehavior.w_selectfn= my_select; // What to do when selected
     widgetbehavior.w_activatefn = my_activate; // What to do when active
     widgetbehavior.w_deletefn = my_delete; // What to do when deleted
     widgetbehavior.w_clickfn = my_click; // What to do when clicked
+    // What about object properties?
+    class_setpropertiesfn(example14_class, my_properties);
+    // How to save its properties with the patch?
+    class_setsavefn(example14_class, my_save);
 
     class_setwidget(example14_class, &widgetbehavior);
 
