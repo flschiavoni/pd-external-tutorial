ab050a316b2e80677deb1d0d01b027f4a9b47bf4
drebs <drebs@riseup.net>
2011-11-15 17:50:10 -0200

    some slides for chapter 5

--- examples/src/example06.c
+++ examples/src/example06.c
@@ -4,48 +4,43 @@
 static t_class *example06_class;
 
 typedef struct _example06 {
-    t_object x_obj;
-    t_float my_float;// Creates a my_float variable into the structure
-    t_outlet *my_outlet; // Defines a outlet
+  t_object x_obj;
+  t_float my_float;
+  t_outlet *my_outlet; // defines an outlet
 } t_example06;
 
 // The BANG method, first inlet
-
-void example06_bang(t_example06 *x) { // The method will always receive the class as argument
-    post("BANGED!");
-    post("My_float value: %f",x->my_float);
-    outlet_bang(x->my_outlet); // Bang my outlet
+void example06_bang(t_example06 *x) {
+  post("BANGED!");
+  post("my_float value: %f",x->my_float);
+  outlet_bang(x->my_outlet); // Bang my outlet
 }
 
 void example06_anything(t_example06 *x, t_symbol *s, int argc, t_atom *argv){
-
-	post("ANYTHING!");
+  post("ANYTHING!");
 }
 
-// Constructos of the class
+// Constructor of the class
 void * example06_new(t_symbol * arg1, t_floatarg arg2) {
-    t_example06 *x = (t_example06 *) pd_new(example06_class);
-    post("First arg: %s", arg1->s_name);
-    post("Second arg: %f", arg2);
-
-    floatinlet_new(&x->x_obj, &x->my_float); //create a passive inlet which value will be stored into my_float variable
-
-    x->my_outlet = outlet_new(&x->x_obj, gensym("bang"));
-
-    return (void *) x;
+  t_example06 *x = (t_example06 *) pd_new(example06_class);
+  post("First arg: %s", arg1->s_name);
+  post("Second arg: %f", arg2);
+  // passive inlet
+  floatinlet_new(&x->x_obj, &x->my_float); 
+  // bang outlet
+  x->my_outlet = outlet_new(&x->x_obj, gensym("bang"));
+  return (void *) x;
 }
 
 void example06_setup(void) {
-    example06_class = class_new(gensym("example06"),
-            (t_newmethod) example06_new, // Constructor
-            0, 
-            sizeof (t_example06),
-	    CLASS_DEFAULT,
-            A_DEFFLOAT, // First Constructor parameter
-            A_DEFSYMBOL, // Second Consctructo parameter
-            0); // LAST argument is ALWAYS zero
-    class_addbang(example06_class, example06_bang); // ADDs an ACTIVE inlet to receive bangs. This method must be linked to a local function.
-    class_addanything(example06_class, example06_anything);
-
+  example06_class = class_new(gensym("example06"),
+    (t_newmethod) example06_new, // Constructor
+    0, sizeof (t_example06), CLASS_DEFAULT,
+    A_DEFFLOAT,  // 1st constructor parameter
+    A_DEFSYMBOL, // 2nd constructor parameter
+    0); // LAST argument is ALWAYS zero
+  // active inlet that receives bangs
+  class_addbang(example06_class, example06_bang); // 
+  class_addanything(example06_class, example06_anything);
 }
 
--- examples/src/example08.c
+++ examples/src/example08.c
@@ -9,65 +9,54 @@ typedef struct _example08 {
     t_outlet *my_outlet; // Defines a outlet
 } t_example08;
 
-
-// Constructos of the class
-void * example08_new(void) {
-    t_example08 *x = (t_example08 *) pd_new(example08_class);
-
-    // creating inlets to the messages "alfa" and "beta"
-    inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("float"), gensym("alfa"));
-    inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("float"), gensym("beta"));
-
-    x->my_outlet = outlet_new(&x->x_obj, gensym("bang"));
-
-    return (void *) x;
-}
-
 // Destroy the class
 void example08_destroy(t_example08 *x) {
    post("You say good bye and I say hello");
 }
 
-
+// Constructor of the class
+void * example08_new(void) {
+  t_example08 *x = (t_example08 *) pd_new(example08_class);
+  // creates inlets for distinct messages
+  inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("float"), gensym("alfa"));
+  inlet_new(&x->x_obj, &x->x_obj.ob_pd, gensym("float"), gensym("beta"));
+  x->my_outlet = outlet_new(&x->x_obj, gensym("bang"));
+  return (void *) x;
+}
 
 void example08_start(t_example08 *x){
-    post("START / BANG");
+  post("START / BANG");
 }
 
 void example08_stop(t_example08 *x){
-    post("STOP");
+  post("STOP");
 }
 
 void example08_open(t_example08 *x, t_symbol *s){
-    post("open %s",s->s_name);
+  post("open %s",s->s_name);
 }
 
-
 void example08_alfa(t_example08 *x, t_floatarg f){
-	post("ALFA VALUE %f",f);
+  post("ALFA VALUE %f",f);
 }
 
 void example08_beta(t_example08 *x, t_floatarg f){
-	post("BETA VALUE %f",f);
+  post("BETA VALUE %f",f);
 }
 
 void example08_setup(void) {
-    example08_class = class_new(gensym("example08"),
-            (t_newmethod) example08_new, // Constructor
-            (t_method) example08_destroy, // Destructor
-            sizeof (t_example08),
-	    CLASS_DEFAULT,
-            0); // LAST argument is ALWAYS zero
-
-    // All these messages will be received by the first left inlet
-    class_addmethod(example08_class, (t_method) example08_start, gensym("start"), 0); // two messages, the same function
-    class_addmethod(example08_class, (t_method) example08_start, gensym("bang"),  0); // may be "start" or "bang" messages
-    class_addmethod(example08_class, (t_method) example08_stop,  gensym("stop"),  0);
-    class_addmethod(example08_class, (t_method) example08_open,  gensym("open"),  A_DEFSYMBOL,0);
-
-    // These messages will be associated with inlets 2 and 3
-    class_addmethod(example08_class, (t_method) example08_alfa,  gensym("alfa"), A_DEFFLOAT,0); 
-    class_addmethod(example08_class, (t_method) example08_beta,  gensym("beta"), A_DEFFLOAT,0); 
-
+  example08_class = class_new(gensym("example08"),
+    (t_newmethod) example08_new, // Constructor
+    (t_method) example08_destroy, // Destructor
+    sizeof (t_example08), CLASS_DEFAULT, 0);
+  // leftmost inlet will trigger methods upon
+  // these messages.
+  class_addmethod(example08_class, (t_method) example08_start, gensym("start"), 0);
+  class_addmethod(example08_class, (t_method) example08_start, gensym("bang"),  0);
+  class_addmethod(example08_class, (t_method) example08_stop, gensym("stop"),  0);
+  class_addmethod(example08_class, (t_method) example08_open, gensym("open"),  A_DEFSYMBOL,0);
+  // associate messages with inlets 2 and 3
+  class_addmethod(example08_class, (t_method) example08_alfa, gensym("alfa"), A_DEFFLOAT,0); 
+  class_addmethod(example08_class, (t_method) example08_beta, gensym("beta"), A_DEFFLOAT,0); 
 }
 
