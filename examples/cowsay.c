e99fccbc6cc066390d6536745826817e633a8fc5
Flavio Schiavoni <fls@rendera.com.br>
2014-06-10 06:37:07 -0300

    - Last update to a seminar

---
+++ examples/cowsay.c
@@ -0,0 +1,13 @@
+#include "m_pd.h"
+
+void cowsay_setup(void){
+   post(" ______");
+   post("< muuu >");
+   post(" ------");
+   post("        \\   ^__^");
+   post("         \\  (oo)\\_______");
+   post("            (__)\\       )\\/\\");
+   post("                ||----w |");
+   post("                ||     ||");
+}
+
